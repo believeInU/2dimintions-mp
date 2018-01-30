63ea6c68c6504c2233fbb5aa07267f02b747337d
believeInU <student@Robotics.k12.il>
2018-01-30 20:24:10 +0200

    add surfceMP
    start working on the main colculation

---
+++ src/surfceMP.java
@@ -0,0 +1,17 @@
+
+public class surfceMP {
+	
+	public surfceMP(Point[] p,double maxVelocity, double a, double d) {
+		
+	}
+	
+	public double[] getV(double x, double y, double angle) {
+		
+		double angle2Robot = angle2start - angle;
+		double vtotal = sumVelocity(x,y);
+		double velocityLeft = vtotal * Math.cos(angle2Robot) - vtotal * Math.sin(angle2Robot) * r / h;
+		double velocityRight = vtotal * Math.cos(angle2Robot) + vtotal * Math.sin(angle2Robot) * r / h;
+		double[] velocity = {velocityLeft, velocityRight};
+		return velocity;
+	}
+}
